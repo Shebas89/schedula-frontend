a4994c942d3b81ad9b0d54e0ae2155a5b8f8d4d4
Sebastian Velandia <sebastian.velandia@blackboard.com>
2021-12-02 10:00:43 -0500

    changing name to components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ErrorComponent } from './component/error/error.component';
-import { FormularioServicioComponent } from './component/formulario-servicio/formulario-servicio.component';
-import { ListServiciosComponent } from './component/list-servicios/list-servicios.component';
-import { NotFoundComponent } from './component/not-found/not-found.component';
-import { ServiciosComponent } from './component/servicios/servicios.component';
-import { TabServiciosComponent } from './component/tab-servicios/tab-servicios.component';
+import { ErrorComponent } from './components/error/error.component';
+import { FormularioServicioComponent } from './components/formulario-servicio/formulario-servicio.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { TabServiciosComponent } from './components/tab-servicios/tab-servicios.component';
 
 const routes: Routes = [
   { path:'servicios', component: ServiciosComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './component/header/header.component';
-import { FooterComponent } from './component/footer/footer.component';
-import { TablaServiciosComponent } from './component/tabla-servicios/tabla-servicios.component';
-import { NotFoundComponent } from './component/not-found/not-found.component';
-import { ServiciosComponent } from './component/servicios/servicios.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TablaServiciosComponent } from './components/tabla-servicios/tabla-servicios.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ServiciosService } from './services/servicios/servicios.service';
-import { ErrorComponent } from './component/error/error.component';
-import { FormularioServicioComponent } from './component/formulario-servicio/formulario-servicio.component';
+import { ErrorComponent } from './components/error/error.component';
+import { FormularioServicioComponent } from './components/formulario-servicio/formulario-servicio.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ListServiciosComponent } from './component/list-servicios/list-servicios.component';
-import { TabServiciosComponent } from './component/tab-servicios/tab-servicios.component';
+import { ListServiciosComponent } from './components/list-servicios/list-servicios.component';
+import { TabServiciosComponent } from './components/tab-servicios/tab-servicios.component';
 
 @NgModule({
   declarations: [
